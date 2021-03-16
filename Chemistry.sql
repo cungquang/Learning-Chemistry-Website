429bc41ffbbe6eeaec21c45dde3049bdc544997e
Hong Quang <hong_quang_cung@sfu.ca>
2021-03-15 22:02:14 -0700

    SQL script - Hong Quang Cung - 03/15/2021

---
+++ Chemistry.sql
@@ -0,0 +1,90 @@
+CREATE DATABASE IF NOT EXISTS Chemistry;
+USE Chemistry;
+
+CREATE TABLE IF NOT EXISTS RegisterUser(
+	ID INT NOT NULL UNIQUE,
+    FirstName CHAR(100) NOT NULL,
+    LastName CHAR(100) NOT NULL,
+    Email VARCHAR(300) NOT NULL,
+    PRIMARY KEY(ID)
+);
+
+INSERT INTO RegisterUser(ID, Firstname, LastName, EmaiL) VALUES(1, "Chris", "Liu", "chris_liu@sfu.ca");
+INSERT INTO RegisterUser(ID, Firstname, LastName, EmaiL) VALUES(2, "Frank", "Dong", "frank_dong@sfu.ca");
+INSERT INTO RegisterUser(ID, Firstname, LastName, EmaiL) VALUES(3, "John", "Minh", "john_minh@sfu.ca");
+INSERT INTO RegisterUser(ID, Firstname, LastName, EmaiL) VALUES(4, "Xixuan", "Liu", "chris_liu@sfu.ca");
+INSERT INTO RegisterUser(ID, Firstname, LastName, EmaiL) VALUES(5, "Hong", "Cung", "hong_cung@sfu.ca");
+SELECT * FROM RegisterUser;
+
+
+CREATE TABLE IF NOT EXISTS Post(
+	PostID INT NOT NULL UNIQUE,
+    PostTitle CHAR(200),
+    PostContent VARCHAR(600) NOT NULL,
+    PostDate DATE NOT NULL,
+    PostTime TIME NOT NULL,
+    PRIMARY KEY (PostID)
+);
+
+INSERT INTO Post(PostID, PostTitle, PostContent, PostDate, PostTime) VALUES(1, "Chris Post", "By Chris Liu", curdate(), curtime());
+INSERT INTO Post(PostID, PostTitle, PostContent, PostDate, PostTime) VALUES(2, "Frank Post", "By Frank Dong", curdate(), curtime());
+INSERT INTO Post(PostID, PostTitle, PostContent, PostDate, PostTime) VALUES(3, "John Post", "By John Minh", curdate(), curtime());
+INSERT INTO Post(PostID, PostTitle, PostContent, PostDate, PostTime) VALUES(4, "Xixuan Post", "By Xixuan", curdate(), curtime());
+INSERT INTO Post(PostID, PostTitle, PostContent, PostDate, PostTime) VALUES(5, "Hong Post", "By Hong", curdate(), curtime());
+
+
+DELETE FROM Post WHERE PostID = 5;
+SELECT * FROM Post;
+
+
+CREATE TABLE IF NOT EXISTS Discuss(
+	PostID INT NOT NULL UNIQUE,
+    ID INT NOT NULL,
+    CreatorName CHAR(200) NOT NULL,
+    PRIMARY KEY (PostID, ID),
+	FOREIGN KEY (PostID) references Post(PostID) ON DELETE CASCADE,
+    FOREIGN KEY (ID) references RegisterUser(ID) ON DELETE CASCADE
+);
+
+
+INSERT INTO Discuss(PostID, ID, CreatorName) VALUES(1, 1,(SELECT FirstName FROM RegisterUser WHERE ID = 1));
+INSERT INTO Discuss(PostID, ID, CreatorName) VALUES(2, 2,(SELECT FirstName FROM RegisterUser WHERE ID = 2));
+INSERT INTO Discuss(PostID, ID, CreatorName) VALUES(4, 3,(SELECT FirstName FROM RegisterUser WHERE ID = 3));
+INSERT INTO Discuss(PostID, ID, CreatorName) VALUES(3, 4,(SELECT FirstName FROM RegisterUser WHERE ID = 4));
+INSERT INTO Discuss(PostID, ID, CreatorName) VALUES(5, 3,(SELECT FirstName FROM RegisterUser WHERE ID = 5));
+
+SELECT * FROM Discuss;
+
+
+
+CREATE TABLE IF NOT EXISTS ReplyComment(
+	CommentID INT NOT NULL,
+    PostID INT NOT NULL,
+    EditorName CHAR(200) DEFAULT "Anonymous",
+    Content VARCHAR(500),
+    CommentDate DATE NOT NULL,
+    CommentTime TIME NOT NULL,
+    PRIMARY KEY(CommentID, PostID),
+    FOREIGN KEY Comment(PostID) references Post(PostID) ON DELETE CASCADE
+);
+
+INSERT INTO ReplyComment(CommentID, PostID, EditorName, Content, CommentDate, CommentTime) VALUES(1, 2, 
+	(SELECT FirstName FROM RegisterUser R WHERE R.ID = 3),"Hello World", curdate(), curtime()); 
+INSERT INTO ReplyComment(CommentID, PostID, EditorName, Content, CommentDate, CommentTime) VALUES(2, 2, 
+	(SELECT FirstName FROM RegisterUser R WHERE R.ID = 4),"Hello World", curdate(), curtime()); 
+INSERT INTO ReplyComment(CommentID, PostID, EditorName, Content, CommentDate, CommentTime) VALUES(3, 3, 
+	(SELECT FirstName FROM RegisterUser R WHERE R.ID = 4),"Hello World", curdate(), curtime()); 
+INSERT INTO ReplyComment(CommentID, PostID, EditorName, Content, CommentDate, CommentTime) VALUES(4, 1, 
+	(SELECT FirstName FROM RegisterUser R WHERE R.ID = 1),"Hello World", curdate(), curtime()); 
+INSERT INTO ReplyComment(CommentID, PostID, Content, CommentDate, CommentTime) VALUES(3, 2, "Hello World", curdate(), curtime()); 
+
+SELECT * FROM ReplyComment;
+
+
+DROP TABLE RegisterUser;
+DROP TABLE Post;
+DROP TABLE Discuss;
+DROP TABLE ReplyComment;
+
+
+DROP DATABASE Chemistry;
