a4adeaf9655079e69855130820a616f5a0ffd7ca
Hong Quang <hong_quang_cung@sfu.ca>
2021-03-25 00:14:46 -0700

    Modify: layout.html forum.html style.css - Create webform.py

--- Webapp/webapp.py
+++ Webapp/webapp.py
@@ -1,44 +1,88 @@
+"""
+*********************************************************************************
+*	Group of authors:															*
+*		Chris Liu																*
+*		Frank Dong																*
+*		John Canh Nhat Minh														*
+*		Hong Quang Cung															*
+*		Xixuan																	*
+*																				*
+*********************************************************************************
+*																				*
+*	File Description:															*
+*		- This file is the core engine of the entire webpage					*
+*		- Each function represent a tab on the webpage, includes:				*
+*			+ def home()														*
+*			+ def search()														*
+*			+ def forum()														*
+*			+ def practice()													*
+*		- Supplemen functions will not includes in this file 					*
+*		- Command to execute the file:											*
+*			- Linux terminal:													*
+*				export FLASK_APP=webapp.py										*
+*				flask run 														*
+*			- Window command line:												*
+*				set FLASK_APP=webapp.py											*
+*				flask run 														*				
+*																				*
+*********************************************************************************
+"""
+
+#----------------------------Library-------------------------------- 
+
 from flask import Flask, render_template, url_for
+from webform import CommentPost
+from datetime import date
 #from flask_sqlalchemy import SQLAlchemy
 
+#Decalre the variable app - main function
 app = Flask(__name__)
 
+#Set Secret key for protectioN
+app.config['SECRET_KEY'] ='f619d6b3556fc0401cf31f077be67042'
+
+
+#------------------------Database management------------------------- 
 #app.config['SQLAlchemy_DATABASE_URI'] = 'sqlite:///forum.db'
 #app.config['SQLAlchemy_TRACK_MODIFICATION'] = False
 app.config['DEBUG'] = True
-#app.config['SECRET_KEY'] = 'mysecret'
+
 
 #db = SQLAlchemy(app)
 #migrate = Migrate(app, db)
 
+
+
+
+#---------------------------Web management----------------------------
+
 @app.route('/')	
+
 
 #Home page
 @app.route('/home')
-def Home():
-	return render_template('home.html')
+def home():
+	return render_template('home.html')		
 
-#Search page
-@app.route('/search')
-def Search():
-	return "<h1>Search.html</h1>"
 
-#Forum page - my part
-@app.route('/forum')
-def Forum():
-	return render_template('forum.html')
+#Forum page 
+@app.route('/forum',methods=['GET','POST'])
+def forum():
+
+	comment = CommentPost()					#Collect user input for the post
+	#if CommentPost.validate_on_submit()			#Validate user input base the assigned condition
+
+	#send the contain the template 
+	return render_template('forum.html', title='Forum', form=comment)	
+
 
 #Practice page
 @app.route('/practice')
-def Practice():
+def practice():
 	return render_template('practice.html')
 
-#Sign-in page
-@app.route('/sign_In')
-def Sign_In():
-	return "<h1>Sign_In.html</h1>"
 
 
-
+#Run the 
 if __name__ == '__main__':
 	app.run(debug=True)
---
+++ Webapp/webform.py
@@ -0,0 +1,43 @@
+"""
+*********************************************************************************
+*	Group of authors:															*
+*		Chris Liu																*
+*		Frank Dong																*
+*		John Canh Nhat Minh														*
+*		Hong Quang Cung															*
+*		Xixuan																	*
+*																				*
+*********************************************************************************
+*																				*
+*	File Description:															*
+*		- This file contains form that will be used on html file				*
+*		- Each function represent a form:										*
+*			+ def home()														*
+*			+ def search()														*
+*			+ def forum()														*
+*			+ def practice()													*
+*		- Supplemen functions will not includes in this file 					*
+*																				*
+*********************************************************************************
+"""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+#CommentPost():
+# this function inherit from FlaskForm
+# Pre-condition: none
+# Post-condition: dispa
+class CommentPost(FlaskForm):
+
+	#variable contains the title of the post
+	postTitle = StringField('postTitle', validators=[DataRequired(), 
+		Length(min=1, max=100)])
+	
+	#variable contains the content of the post
+	postContent = StringField('postContent',validators=[DataRequired(), 
+		Length(min=1, max=500)])
+	
+	#variable to submit the post
+	post = SubmitField('post')
