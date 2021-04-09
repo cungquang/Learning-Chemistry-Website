1e4d92d8e4b6c0abb256daa49d6b9749083b0683
Chris <46358176+chrisliu0424@users.noreply.github.com>
2021-04-09 20:31:08 +0800

    Added Comments

--- Project/webapp/search_data.py
+++ Project/webapp/search_data.py
@@ -1,8 +1,14 @@
 from webapp import db
 
 #-------------------------------ReplyComment table-------------------------------- 
+# Compound():
+# Pre-condition: None
+# Post-condition: construct the table of compound information
 class Compound(db.Model):
     __tablename__ = "compound"
+  
+    # Construct attributes, primary key
+    # Primary key: CompoundName, ChemicalFormula
     CompoundName = db.Column(db.String(30), primary_key = True, nullable = False)
     ChemicalFormula = db.Column(db.String(30), primary_key = True, nullable = False)
     AtomicNumber = db.Column(db.Integer())
@@ -16,14 +22,22 @@ class Compound(db.Model):
         return f"Compound('{self.CompoundName}','{self.ChemicalFormula}','{self.AtomicNumber}','{self.State}','{self.MeltingPoint}','{self.BoilingPoint}','{self.Appearance}','{self.MolecularWeight}')"
 
 #-------------------------------ReplyComment table-------------------------------- 
+# Produces():
+# Pre-condition: Compound table exists
+# Post-condition: construct the table of produces
 class Produces(db.Model):
     __tablename__ = "produces"
+  
+    # Construct attributes, primary key and foreign key 
+    # Primary key: ReactantFormula, ProductFormula
+    # Foreign key:
+    # Products.ReactantFormula references Compound.ChemicalFormula
+    # Products.ProductFormula references Compound.ChemicalFormula
     ReactantFormula = db.Column(db.String(30), db.ForeignKey('compound.ChemicalFormula'), primary_key = True, nullable = False)
     ProductFormula = db.Column(db.String(30), db.ForeignKey('compound.ChemicalFormula'), primary_key = True, nullable = False)
-    ChemicalEquation = db.Column(db.String(100), nullable = False)
-    #Setup foreign key: Products.ReactantFormula references Compound.ChemicalFormula
-    #                   Products.ProductFormula references Compound.ChemicalFormula
+    ChemicalEquation = db.Column(db.String(100), nullable = False
 
+    # Enable to interact with the data from Compound
     compound_ref = db.relationship('Compound', foreign_keys = [ReactantFormula])
     product_ref = db.relationship('Compound', foreign_keys = [ProductFormula])
 
@@ -31,13 +45,21 @@ class Produces(db.Model):
         return f"Producesc('{self.ReactantFormula}','{self.ProductFormula}','{self.ChemicalEquation}')"
 
 #-------------------------------ReplyComment table-------------------------------- 
+# Search():
+# Pre-condition: Compound, RegisterUser tables exist
+# Post-condition: construct the table of search
 class Search(db.Model):
     __tablename__ = "search"
+    
+    # Construct attributes, primary key and foreign key 
+    # Primary key: CompoundFormula, UserID
+    # Foreign key:
+    # Search.CompoundFormula references Compound.ChemicalFormula
+    # Search.UserID references User.UserID
     CompoundFormula = db.Column(db.String(30), db.ForeignKey('compound.ChemicalFormula'), primary_key = True, nullable = False)
     UserID = db.Column(db.Integer, db.ForeignKey('registeruser.id'), primary_key = True, nullable = False)
 
-    # Setup foreign key: Search.CompoundFormula references Compound.ChemicalFormula
-    #                    Search.UserID references User.UserID
+    # Enable to interact with the data from Compound and RegisterUser
     search = db.relationship('Compound',lazy = True)
     search = db.relationship('RegisterUser',lazy = True)
 
@@ -45,14 +67,22 @@ class Search(db.Model):
         return f"Search('{self.CompoundFormula}','{self.UserID}')"
 
 #-------------------------------ReplyComment table-------------------------------- 
+# SearchHistory():
+# Pre-condition: Compound, RegisterUser tables exist
+# Post-condition: construct the table of search
 class SearchHistory(db.Model):
     __tablename__ = "searchhistory"
+
+    # Construct attributes, primary key and foreign key 
+    # Primary key: CompoundFormula, UserID
+    # Foreign key:
+    # SearchHistory.CompoundFormula references Compound.ChemicalFormula
+    # SearchHistory.UserID references User.UserID
     CompoundFormula = db.Column(db.String(30), db.ForeignKey('compound.ChemicalFormula'), primary_key = True, nullable = False)
     UserID = db.Column(db.Integer, db.ForeignKey('registeruser.id'), primary_key = True, nullable = False)
     KeywordHistory = db.Column(db.String(100))
 
-    # Setup foreign key: Search.CompoundFormula references Compound.ChemicalFormula
-    #                    Search.UserID references User.UserID
+    # Enable to interact with the data from Compound and RegisterUser
     searchhistory = db.relationship('Compound',lazy = True)
     searchhistory = db.relationship('RegisterUser',lazy = True)
 
