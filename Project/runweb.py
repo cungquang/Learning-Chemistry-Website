28176049c5498bc5e32459ddc3a37eafd029fa74
Hong Quang <44489511+cungquang@users.noreply.github.com>
2024-01-12 22:10:11 -0800

    Update runweb.py

--- Project/runweb.py
+++ Project/runweb.py
@@ -1,24 +1,15 @@
 """
 *********************************************************************************
-*	Group of authors:															*
-*		Chris Liu																*
-*		Frank Dong																*
-*		John Canh Nhat Minh														*
-*		Hong Quang Cung															*
-*		Xixuan																	*
-*																				*
-*********************************************************************************
-*																				*
-*	File Description:															*
-*		- This file is running the package webapp								*
-*		- Command to execute the file:											*
-*			- Linux terminal:													*
-*				python3 runweb.py 												*
-*				python runweb.py 												*
-*			- Window command line:												*
-*				python3 runweb.py 												*
-8				python3 runweb.py 												*
-*																				*																				*
+*	File Description:							*
+*		- This file is running the package webapp			*
+*		- Command to execute the file:					*
+*			- Linux terminal:					*
+*				python3 runweb.py 				*
+*				python runweb.py 				*
+*			- Window command line:					*
+*				python3 runweb.py 				*
+8				python3 runweb.py 				*
+*										*																				*
 *********************************************************************************
 """
 
